eb19e03d09c61c5959f19fe8742b6a3532e2b6ff
Adelina Stancioi <adelina.stancioi@yahoo.com>
2020-03-29 21:32:14 +0300

    Add project files.

---
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor
+{
+    public class EditModel : PageModel
+    {
+        public readonly ApplicationDbContext db;
+
+        public EditModel(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task OnGet(int id)
+        {
+            Book = await db.Book.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookEdit = await db.Book.FindAsync(Book.Id);
+                BookEdit.Name = Book.Name;
+                BookEdit.Author = Book.Author;
+                BookEdit.ISBN = Book.ISBN;
+
+                await db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
---
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+        public IndexModel(ApplicationDbContext db)
+        {
+            this.applicationDbContext = db;
+        }
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGetAsync()
+        {
+            Books = await applicationDbContext.Book.ToListAsync();
+        }
+    }
+}
